fc7775ad00d8620f51b65733a80b69403cf51be1
26735jmc <13514717+jmcoleman@users.noreply.github.com>
2018-10-03 20:29:25 -0400

    heroku

--- server.js
+++ server.js
@@ -7,4 +7,22 @@ app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express, Jenni' });
 });
 
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
+
+//////////////////////////////////////////////////////////
+//default for when no other route is hit.  Must be last
+//////////////////////////////////////////////////////////
+app.get('*', (req, res) => {
+    res.send({ something: 'Hello Hello' })
+})
+
+// listen for request
 app.listen(port, () => console.log(`Listening on port ${port}`));
