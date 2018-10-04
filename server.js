511fd8231867403ef92035a524927c4f2adefbc1
26735jmc <13514717+jmcoleman@users.noreply.github.com>
2018-10-03 21:32:32 -0400

    hook to mongo with basic scrape

--- server.js
+++ server.js
@@ -1,9 +1,66 @@
-const express = require('express');
-var path = require("path");
+/////////////////////////////////////////////////////////
+// handle environment variables
+/////////////////////////////////////////////////////////
 
+// use dotenv to read .env vars into Node but silence the Heroku log error for production as no .env will exist
+require('dotenv').config( { silent: process.env.NODE_ENV === 'production' } );
+
+// process.env.NODE_ENV is set by heroku with a default value of production
+if (process.env.NODE_ENV === 'production') {
+    console.log("in PROD");
+    // connect to the MongoDB on heroku using MONGODB_URI below
+  } else {
+    console.log("in DEV");
+    // use the connection info from the .env file otherwise
+    require('dotenv').load();
+  }
+
+//////////////////////////
+// dependencies
+//////////////////////////
+const mongoose = require("mongoose");
+const express = require('express');
+const session = require('express-session');
+const path = require("path");
+const bodyParser = require("body-parser");
+
+///////////////////////
+// configure Express
+///////////////////////
 const app = express();
+app.use(express.json());
+
 const port = process.env.PORT || 5000;
 
+// body parsing middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+/////////////////////////
+// connect to Mongo DB
+/////////////////////////
+// If deployed, use the deployed database. Otherwise use the local nytreact database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nytreact";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.set('useCreateIndex', true)
+
+/////////////////////
+// Expess Session
+/////////////////////
+// console.log("secret: " +  process.env.SECRET_KEY);
+app.use(session({
+    secret: process.env.SECRET_KEY,     // put this in the heroku environment variables
+    saveUninitialized: true,
+    resave: true
+  }));
+
+//////////////////////
+// initial route
+//////////////////////
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express, Jenni' });
 });
@@ -17,6 +74,12 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
 }
+
+////////////////////////////////////////////////////////
+// Import routes and give the server access to them.
+////////////////////////////////////////////////////////
+var routes = require("./controllers/app_controller.js");
+app.use(routes);
 
 //////////////////////////////////////////////////////////
 //default for when no other route is hit.  Must be last
